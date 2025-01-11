7ad19f4b203978908df80a1cd8e5f60dbf454865
kidirbayevamirxon <amirxonkidirbayev0@gmail.com>
2025-01-11 17:09:44 +0500

    dfgdfdsf

--- src/components/section1/section.jsx
+++ src/components/section1/section.jsx
@@ -6,22 +6,22 @@ import img2 from "../../assets/images (1).jpg";
 const mahsulotlar = [
   {
     img: img,
-    nomi: "ECSPERT",
-    tavsif:
+    name: "ECSPERT",
+    incruduction:
       "Bu bosh terisini va sochlarni yuvish uchun ishlatiladigan kosmetik vosita. U sochlarni tozalash, ortiqcha yog'larni olib tashlash va bosh terisini sog'lomlashtirish maqsadida ishlatiladi.",
     sena: "$12.00",
   },
   {
     img: img1,
-    nomi: "LOREAL",
-    tavsif:
+    name: "LOREAL",
+    incruduction:
       "Tarkibi: Shampuanlar odatda suv, detarjanlar (yuvish vositalari), pH muvozanatli moddalar va ba'zan vitaminlar va ozuqa moddalarini o'z ichiga oladi.",
     sena: "$13.99",
   },
   {
     img: img2,
-    nomi: "ELSEVE",
-    tavsif:
+    name: "ELSEVE",
+    incruduction:
       "Bu bosh terisini va sochlarni yuvish uchun ishlatiladigan kosmetik vosita. U sochlarni tozalash, ortiqcha yog'larni olib tashlash va bosh terisini sog'lomlashtirish maqsadida ishlatiladi.",
     sena: "$14.99",
   },
@@ -36,8 +36,8 @@ export function Section() {
           {mahsulotlar.map((mahsulot, index) => (
             <div key={index} className={classess.homeDiv1}>
               <img src={mahsulot.img} className={classess.homeImg} />
-              <h2 className={classess.name}>{mahsulot.nomi}</h2>
-              <p className={classess.incruduction}>{mahsulot.tavsif}</p>
+              <h2 className={classess.name}>{mahsulot.name}</h2>
+              <p className={classess.incruduction}>{mahsulot.incruduction}</p>
               <span className={classess.sena}>{mahsulot.sena}</span>
             </div>
           ))}
